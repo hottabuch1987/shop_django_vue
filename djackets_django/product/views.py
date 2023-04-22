c81c65452a0983130c172c487c20785a3cb5133c
hottabych <varvar1987a@mail.ru>
2023-04-22 16:30:05 +0300

    Add functionality to vuex

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,28 +1,26 @@
-from django.http import Http404
-
+from itertools import product
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from .models import Product
-from .serializers import ProductSerailizer
+from .serializers import ProductSerializer
+from django.http import Http404
 
 
-class LatestProductList(APIView):
+class LatestProductsList(APIView):
     def get(self, request, format=None):
-        product = Product.objects.all()[0:4]
-        serializer = ProductSerailizer(product, many=True)
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-class ProductDetail(APIView):
 
+class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
-
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
-        serializer = ProductSerailizer(product)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
-
