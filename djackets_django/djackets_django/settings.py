2554a045acf1c86c95787a90ea5b8eaa5e597645
hottabych <varvar1987a@mail.ru>
2023-04-22 11:05:17 +0300

    Create django app and models for products

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -128,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
